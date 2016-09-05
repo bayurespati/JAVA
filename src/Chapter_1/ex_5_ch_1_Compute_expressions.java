875f2b801788dfc82b77ce8019e800fa8f9a8e04
OmniKnight <omniknight@gmail.com>
2016-09-05 16:32:20 +0700

    ex_5_ch_1 Compute expressions

---
+++ src/Chapter_1/ex_5_ch_1_Compute_expressions.java
@@ -0,0 +1,12 @@
+package Chapter_1;
+
+/**
+ * Created by DWP on 9/5/16.
+ * Compute expressions) Write a program that displays the result of 9.5 * 4.5 - 2.5 * 3.
+ 45.5 - 3.5
+ */
+public class ex_5_ch_1_Compute_expressions {
+    public static void main(String[] args) {
+        System.out.print( ((9.5*4.5)-(2.5*3)) / (45.5-3.5));
+    }
+}
