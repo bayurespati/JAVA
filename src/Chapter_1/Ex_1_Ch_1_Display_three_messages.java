e0ce7eb32b609cf740fd585a0f3a36ace0c14500
OmniKnight <omniknight@gmail.com>
2016-09-05 15:43:33 +0700

    ex_1_ch_1 Display three messages

---
+++ src/Chapter_1/Ex_1_Ch_1_Display_three_messages.java
@@ -0,0 +1,14 @@
+package Chapter_1;
+
+/**
+ * Created by DWP on 9/5/16.
+ *
+ * (Display three messages) Write a program that displays Welcome to Java, Welcome to Computer Science, and Programming is fun.
+ */
+public class Ex_1_Ch_1_Display_three_messages {
+    public static void main(String[] args){
+        System.out.print("Welcome to java\n");
+        System.out.print("Welcome to Computer Science\n");
+        System.out.print("Programming is fun");
+    }
+}
