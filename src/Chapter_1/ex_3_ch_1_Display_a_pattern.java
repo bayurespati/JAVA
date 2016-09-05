5ae0399e37eb1ce2f7ffe610751ee8649a95cd51
OmniKnight <omniknight@gmail.com>
2016-09-05 16:02:39 +0700

    ex_3_ch_1 Display a pattern

---
+++ src/Chapter_1/ex_3_ch_1_Display_a_pattern.java
@@ -0,0 +1,18 @@
+package Chapter_1;
+
+/**
+ * Created by DWP on 9/5/16.
+ *
+ * (Display a pattern) Write a program that displays the following pattern:
+ JAVVA
+ J AA V V AA J J AAAAA V V AAAAA
+ JJ A A V A A
+ */
+public class ex_3_ch_1_Display_a_pattern {
+    public static void main(String[] args) {
+        System.out.print("   j    A    v     v    A    \n");
+        System.out.print("   j   A A    v   v    A A    \n");
+        System.out.print("j  j  AAAAA    v v    AAAAA    \n");
+        System.out.print(" jj  A     A    v    A     A");
+    }
+}
