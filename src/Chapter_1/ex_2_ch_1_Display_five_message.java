9066ba32a3c1998426810bd972a87bc90ecce722
OmniKnight <omniknight@gmail.com>
2016-09-05 15:51:00 +0700

    ex_2_ch_1 Display five message

---
+++ src/Chapter_1/ex_2_ch_1_Display_five_message.java
@@ -0,0 +1,15 @@
+package Chapter_1;
+
+/**
+ * Created by DWP on 9/5/16.
+ * (Display five messages) Write a program that displays Welcome to Java five times.
+ */
+public class ex_2_ch_1_Display_five_message {
+    public static void main(String[] args) {
+        System.out.print("Welcome to Java\n");
+        System.out.print("Welcome to Java\n");
+        System.out.print("Welcome to Java\n");
+        System.out.print("Welcome to Java\n");
+        System.out.print("Welcome to Java");
+    }
+}
