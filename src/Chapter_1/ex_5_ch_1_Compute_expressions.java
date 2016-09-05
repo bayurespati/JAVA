57dbaa847a23a0fb87690096795111529ec8334c
OmniKnight <omniknight@gmail.com>
2016-09-05 16:50:38 +0700

    ex_6_ch_1 summtion of a series and refine ex_5_ch_1

--- src/Chapter_1/ex_5_ch_1_Compute_expressions.java
+++ src/Chapter_1/ex_5_ch_1_Compute_expressions.java
@@ -7,6 +7,6 @@ package Chapter_1;
  */
 public class ex_5_ch_1_Compute_expressions {
     public static void main(String[] args) {
-        System.out.print( ((9.5*4.5)-(2.5*3)) / (45.5-3.5));
+        System.out.print ("((9.5*4.5)-(2.5*3)) / (45.5-3.5) = " + ((9.5*4.5)-(2.5*3)) / (45.5-3.5));
     }
 }
---
+++ src/Chapter_1/ex_6_ch_1_Summtion_of_a_series.java
@@ -0,0 +1,13 @@
+package Chapter_1;
+
+/**
+ * Created by DWP on 9/5/16.
+ (Summation of a series) Write a program that displays the result of
+ 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+ */
+public class ex_6_ch_1_Summtion_of_a_series {
+    public static void main(String[] args) {
+
+        System.out.print("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " +(1+2+3+4+5+6+7+8+9)   );
+    }
+}
