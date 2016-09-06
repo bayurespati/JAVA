ef4b24a366770ee4061b1c0a38071474677e8b8d
OmniKnight <omniknight@gmail.com>
2016-09-06 17:16:25 +0700

    eg_7_ch_2 show current time

---
+++ src/Chapter_2/eg_7_ch_2_Show_current_time.java
@@ -0,0 +1,18 @@
+package Chapter_2;
+
+/**
+ * Created by DWP on 9/6/16.
+ */
+public class eg_7_ch_2_Show_current_time {
+    public static void main(String[] args) {
+        long totalMilliSecond = System.currentTimeMillis();
+        long totalSecond = totalMilliSecond / 1000;
+        long currentSecond = totalSecond % 60;
+        long totalMinutes = totalSecond / 60;
+        long currentMinutes = totalMinutes % 60;
+        long totalHour = totalMinutes / 60;
+        long currentHour = totalHour % 24;
+
+        System.out.print("Current time is   "+currentHour+" : "+currentMinutes+" : "+currentSecond+ " GMT");
+    }
+}
