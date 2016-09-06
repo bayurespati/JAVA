7b674b62f910229e56352c3be11a4990648e70bb
OmniKnight <omniknight@gmail.com>
2016-09-06 13:44:13 +0700

    eg_1_ch_2 listing

---
+++ src/Chapter_2/eg_1_ch_2_listing.java
@@ -0,0 +1,13 @@
+package Chapter_2;
+
+/**
+ * Created by DWP on 9/6/16.
+ */
+public class eg_1_ch_2_listing {
+    public static void main(String[] args) {
+        double radius= 20;
+        double area=  radius * radius * Math.PI;
+
+        System.out.println("the area of circle of radius "+ radius + "is = " + area);
+    }
+}
