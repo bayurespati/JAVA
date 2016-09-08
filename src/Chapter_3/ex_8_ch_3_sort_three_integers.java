dfe6afd3cb628e81cb7f526f090f846d12d87c3e
OmniKnight <omniknight@gmail.com>
2016-09-09 00:05:06 +0700

    ex_8_ch_3 sort three integers

---
+++ src/Chapter_3/ex_8_ch_3_sort_three_integers.java
@@ -0,0 +1,38 @@
+package Chapter_3;
+
+import java.util.Scanner;
+
+/**
+ * Created by DWP on 9/8/16.
+ *
+ */
+public class ex_8_ch_3_sort_three_integers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter three integer : ");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        int number3 = input.nextInt();
+
+
+        if(number1>number2){
+            int tempt = number1;
+            number1 = number2;
+            number2 = tempt;
+        }
+        if(number1>number3){
+            int temp = number1;
+            number1 = number3;
+            number3 = temp;
+        }
+        if(number2>number3){
+            int temp = number2;
+            number2 = number3;
+            number3 = temp;
+        }
+
+
+        System.out.println(" the number is : "+number1+" "+number2+" "+number3);
+    }
+}
