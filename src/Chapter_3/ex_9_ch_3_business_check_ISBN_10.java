86324282366d2d2ca3e51dbe6377cbbee4b3b055
OmniKnight <omniknight@gmail.com>
2016-09-09 15:47:04 +0700

    ex_9_ch_3 refactor business check ISBN

--- src/Chapter_3/ex_9_ch_3_business_check_ISBN_10.java
+++ src/Chapter_3/ex_9_ch_3_business_check_ISBN_10.java
@@ -14,15 +14,17 @@ public class ex_9_ch_3_business_check_ISBN_10 {
 
 
         System.out.print("Enter the first 9 digit of an ISBN as integer: ");
-        int number1 = input.nextInt();
-        int number2 = input.nextInt();
-        int number3 = input.nextInt();
-        int number4 = input.nextInt();
-        int number5 = input.nextInt();
-        int number6 = input.nextInt();
-        int number7 = input.nextInt();
-        int number8 = input.nextInt();
-        int number9 = input.nextInt();
+        int number = input.nextInt();
+
+        int number9 = number % 10;
+        int number8 = (number % 100 - number9) / 10;
+        int number7 = (number % 1000 - number8) /100;
+        int number6 = (number % 10000 - number7) /1000;
+        int number5 = (number % 100000 - number6) /10000;
+        int number4 = (number % 1000000 - number5) /100000;
+        int number3 = (number % 10000000 - number4) /1000000;
+        int number2 = (number % 100000000 - number3) /10000000;
+        int number1 = (number % 1000000000 - number2) /100000000;
 
         int checkSum = (number1 * 1 + number2 * 2 + number3 * 3 + number4 *
                 4 + number5 * 5 + number6 * 6 +number7 * 7 +number8* 8 + number9*9 )%11;
@@ -39,7 +41,7 @@ public class ex_9_ch_3_business_check_ISBN_10 {
             case 7 : System.out.print("7"); break;
             case 8 : System.out.print("8"); break;
             case 9 : System.out.print("9"); break;
-            case 10 : System.out.print("x");
+            case 10 : System.out.print("X");
         }
     }
 }
