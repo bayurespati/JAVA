61470b38ee805429283c55a4eec22284833cf32e
OmniKnight <omniknight@gmail.com>
2016-09-27 15:01:27 +0700

    eg_2_ch_5 guess number one time

--- src/Chapter_5/eg_2_ch_5_guess_number_one_time.java
+++ src/Chapter_5/eg_2_ch_5_guess_number_one_time.java
@@ -1,7 +1,27 @@
 package Chapter_5;
 
+import java.util.Scanner;
+
 /**
  * Created by DWP on 9/27/16.
+ *
  */
 public class eg_2_ch_5_guess_number_one_time {
+    public static void main(String[] args) {
+        int number = (int)(Math.random()*101);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a magic number between 0 and 100");
+
+        System.out.printf("\nEnter your guess: ");
+        int guess = input.nextInt();
+
+
+            if (guess == number)
+                System.out.println("Yes, the number is " + number);
+            else if (guess > number)
+                System.out.println("Your guess is too high");
+            else
+                System.out.printf("Your guess is too low");
+    }
 }
