68465eaea24c8738800e02b6e038503be74303a3
OmniKnight <omniknight@gmail.com>
2016-09-27 15:04:24 +0700

    eg_4_5 subtraction quiz

--- src/Chapter_5/eg_4_ch_5_subtraction_quiz_loop.java
+++ src/Chapter_5/eg_4_ch_5_subtraction_quiz_loop.java
@@ -1,7 +1,49 @@
 package Chapter_5;
 
+import java.util.Scanner;
+
 /**
  * Created by DWP on 9/27/16.
+ *
  */
 public class eg_4_ch_5_subtraction_quiz_loop {
+    public static void main(String[] args) {
+        final int NUMBER_OF_QUESTIONS = 5;
+
+        int correctCount = 0;
+        int count = 0;
+
+        long startTime = System.currentTimeMillis();
+        String output = " ";
+        Scanner input = new Scanner(System.in);
+
+        while (count < NUMBER_OF_QUESTIONS){
+            int number1 = (int) (Math.random()*10);
+            int number2 = (int) (Math.random()*10);
+
+
+            if(number1 < number2){
+                int temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+            System.out.print("what is "+number1+" - "+number2+" ? ");
+            int answer = input.nextInt();
+
+            if(number1 - number2 == answer){
+                System.out.println("You are correct!");
+                correctCount++;
+            }
+            else
+                System.out.println("what is "+number1+" - "+number2+"Should be "+(number1-number2));
+
+            count++;
+            output +="\n"+number1+"-"+number2+"="+answer+((number1 - number2 == answer)?"correct":"wrong");
+        }
+
+        long endTime = System.currentTimeMillis();
+        long testTime = endTime - startTime;
+
+        System.out.println("Correct count is "+correctCount+"\nTest time is "+testTime/1000+"second\n"+output);
+    }
 }
--- src/Chapter_5/eg_5_ch_5_sentinel_value.java
+++ src/Chapter_5/eg_5_ch_5_sentinel_value.java
@@ -1,7 +1,24 @@
 package Chapter_5;
 
+import java.util.Scanner;
+
 /**
  * Created by DWP on 9/27/16.
+ *
  */
 public class eg_5_ch_5_sentinel_value {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an integer (the input ends if it is 0): ");
+        int data = input.nextInt();
+
+        int sum = 0;
+        while(data !=0){
+            sum += data;
+            System.out.print("Enter an integer (the input ends if it is 0): ");
+            data = input.nextInt();
+        }
+        System.out.println("The sum is "+sum);
+    }
 }
