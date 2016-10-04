88007c7be51507eefe3aae20dfa5033f27423854
OmniKnight <omniknight@gmail.com>
2016-10-04 11:50:58 +0700

    ex_24_ch_5 sum a series refine

--- src/Chapter_5/ex_24_ch_5_sum_a_series.java
+++ src/Chapter_5/ex_24_ch_5_sum_a_series.java
@@ -4,4 +4,11 @@ package Chapter_5;
  * Created by DWP on 10/3/16.
  */
 public class ex_24_ch_5_sum_a_series {
+    public static void main(String[] args) {
+        double z = 0;
+        for(double i = 97, j = 99; i >= 1; i -=2, j -=2){
+            z += i/j;
+        }
+        System.out.println(z);
+    }
 }
