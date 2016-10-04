b29279e173f675e37728a43ada0456251e4b4f5f
OmniKnight <omniknight@gmail.com>
2016-10-04 11:51:59 +0700

    ex_25_ch_5 compute p

--- src/Chapter_5/ex_25_ch_5_compute_p.java
+++ src/Chapter_5/ex_25_ch_5_compute_p.java
@@ -4,4 +4,15 @@ package Chapter_5;
  * Created by DWP on 10/3/16.
  */
 public class ex_25_ch_5_compute_p {
+    public static void main(String[] args) {
+      for(double z = 10000; z <= 100000; z +=10000) {
+          double p1 = 0;
+          for (double i = z; i >= 2; i--) {
+              p1 += Math.pow(-1, (i + 1)) / ((2 * i) - 1);
+          }
+          double p = (p1 + 1)*4;
+          System.out.println("i " + z + " p = " + p);
+      }
+
+    }
 }
