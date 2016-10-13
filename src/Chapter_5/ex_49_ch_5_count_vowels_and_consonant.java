dd2fb7b2530b45d1f06a970044f2a39249bd3f15
OmniKnight <omniknight@gmail.com>
2016-10-13 20:10:06 +0700

    ex_49_ch_5 count vowels and consonant

---
+++ src/Chapter_5/ex_49_ch_5_count_vowels_and_consonant.java
@@ -0,0 +1,35 @@
+package Chapter_5;
+
+import java.util.Scanner;
+
+/**
+ * Created by DWP on 10/13/16.
+ */
+public class ex_49_ch_5_count_vowels_and_consonant {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a String : ");
+        String string = input.nextLine();
+
+        String vocal = "aiueoAIUEO";
+        String consonant = "bcdfghjklmnpqrsBCDFGHJKLMNPQRS";
+
+        int vowelCount = 0;
+        int consonantCount = 0;
+        for(int i = 0; i < string.length(); i++){
+            String letter = "";
+            letter += string.charAt(i);
+
+            if(vocal.contains(letter)){
+                vowelCount ++;
+            }
+            if(consonant.contains(letter)){
+                consonantCount ++;
+            }
+
+        }
+        System.out.println("The number of vowel is : "+vowelCount);
+        System.out.println("The number of consonant is : "+consonantCount);
+    }
+}
