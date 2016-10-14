9493afc6df2420ae631bca1329b2863d85366143
OmniKnight <omniknight@gmail.com>
2016-10-14 15:45:32 +0700

    eg_3_ch_6 test return grade method

---
+++ src/Chapter_6/eg_3_ch_6_test_return_grade_method.java
@@ -0,0 +1,24 @@
+package Chapter_6;
+
+/**
+ * Created by DWP on 10/14/16.
+ */
+public class eg_3_ch_6_test_return_grade_method {
+    public static void main(String[] args) {
+        System.out.println("The grade is "+getGrade(78.5));
+        System.out.printf("The grade is "+getGrade(59.5));
+    }
+
+    public static char getGrade(double score){
+        if(score >= 90.0)
+            return 'A';
+        else if(score >= 80.0)
+            return 'B';
+        else if(score >= 70.0)
+            return 'C';
+        else if(score >= 60.0)
+            return 'D';
+        else
+            return 'F';
+    }
+}
