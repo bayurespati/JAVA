b7ec7ff59a6656e86abb2f7de4848ac46e119d6c
OmniKnight <omniknight@gmail.com>
2016-10-24 14:04:31 +0700

    eg_10_ch_6 random character

---
+++ src/Chapter_6/eg_10_ch_6_random_character.java
@@ -0,0 +1,26 @@
+package Chapter_6;
+
+/**
+ * Created by DWP on 10/24/16.
+ */
+public class eg_10_ch_6_random_character {
+        public static char getRandomCharacter(char ch1, char ch2){
+            return (char)(ch1+Math.random()*(ch2 - ch1 + 1));
+        }
+
+        public static char getRandomLowerCaseLetter(){
+            return getRandomCharacter('a','z');
+        }
+
+        public static char getRandomUpperCaseLetter(){
+            return  getRandomCharacter('A','Z');
+        }
+
+        public static char getRandomDigitCharacter(){
+            return getRandomCharacter('0','9');
+        }
+
+        public static char getRandomCharacter(){
+            return getRandomCharacter('\u0000','\uFFFF');
+        }
+}
