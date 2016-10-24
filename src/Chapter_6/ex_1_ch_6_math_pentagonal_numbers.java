b2379b7622a624f4cbc30dfd968c277548e9349c
OmniKnight <omniknight@gmail.com>
2016-10-24 19:58:12 +0700

    ex_1_ch_6 math pentagonal numbers

--- src/Chapter_6/ex_1_ch_6_math_pentagonal_numbers.java
+++ src/Chapter_6/ex_1_ch_6_math_pentagonal_numbers.java
@@ -4,4 +4,22 @@ package Chapter_6;
  * Created by DWP on 10/24/16.
  */
 public class ex_1_ch_6_math_pentagonal_numbers {
+    public static void main(String[] args) {
+        printPentagonalNumber();
+    }
+
+    public static void printPentagonalNumber(){
+        for(int i = 1; i <= 100; i++){
+            System.out.print(" "+getPentagonalNumber(i));
+            if(i % 10 == 0){
+                System.out.println();
+            }
+        }
+    }
+
+    public static int getPentagonalNumber(int i){
+        i = (i * (3*i-1))/2;
+        return i;
+    }
+
 }
