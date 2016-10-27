f548439964db219ba7c98b3a651b6991429c6374
OmniKnight <omniknight@gmail.com>
2016-10-27 19:43:14 +0700

    ex_22_ch_6 approximate the square root

--- src/Chapter_6/ex_22_ch_6_approximate_the_square_root.java
+++ src/Chapter_6/ex_22_ch_6_approximate_the_square_root.java
@@ -1,7 +1,35 @@
 package Chapter_6;
 
+import java.util.Scanner;
+
 /**
  * Created by DWP on 10/27/16.
  */
 public class ex_22_ch_6_approximate_the_square_root {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        printRoot(input.nextLong());
+    }
+
+    public static void printRoot(long number){
+        System.out.println(sqrt(number));
+    }
+
+    public static double sqrt(long n){
+
+        double lastGuest = 1;
+        double nextGuest = (lastGuest + (n / lastGuest)) /2;
+
+        while(isRoot(lastGuest,nextGuest)) {
+            lastGuest = nextGuest;
+            nextGuest = (lastGuest + (n / lastGuest)) /2;
+        }
+        return lastGuest;
+    }
+
+    public static boolean isRoot(double lastGuest, double nextGuest){
+        return nextGuest != lastGuest ||
+                nextGuest - lastGuest >= 0.00001;
+    }
 }
