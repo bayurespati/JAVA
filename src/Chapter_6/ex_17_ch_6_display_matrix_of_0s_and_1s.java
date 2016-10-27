b38a4202f5af190223f2c64edcfdeca0e712186a
OmniKnight <omniknight@gmail.com>
2016-10-27 11:17:09 +0700

    ex_17_ch_6 display matrix of 0s and 1s

--- src/Chapter_6/ex_17_ch_6_display_matrix_of_0s_and_1s.java
+++ src/Chapter_6/ex_17_ch_6_display_matrix_of_0s_and_1s.java
@@ -1,7 +1,25 @@
 package Chapter_6;
 
+import java.util.Scanner;
+
 /**
  * Created by DWP on 10/27/16.
  */
 public class ex_17_ch_6_display_matrix_of_0s_and_1s {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        printMatrixN(input.nextInt());
+    }
+
+    public static void printMatrixN(int n){
+        for(int i = n; i > 0; i --){
+            for(int k = n; k > 0; k--) {
+                int m = (int) (Math.random() * 2);
+                System.out.print(m);
+            }
+            System.out.println();
+        }
+
+    }
 }
