c5953088e9fafea34df8694d1a3b2013183b9cd7
OmniKnight <omniknight@gmail.com>
2016-11-02 11:41:17 +0700

    ex_36_ch_6 area of regular polygon

---
+++ src/Chapter_6/ex_36_ch_6_area_of_regular_polygon.java
@@ -0,0 +1,27 @@
+package Chapter_6;
+
+import java.util.Scanner;
+
+/**
+ * Created by DWP on 11/2/16.
+ */
+public class ex_36_ch_6_area_of_regular_polygon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter The number of side : ");
+        int numberOfSide = input.nextInt();
+        System.out.print("Enter The side : ");
+        double side = input.nextDouble();
+
+        printAreaOfPolygon(numberOfSide,side);
+    }
+
+    public static void printAreaOfPolygon(int numberOfSide, double side){
+        System.out.println("The area of polygon is : "+computeAreaOfPolygon(numberOfSide,side));
+    }
+
+    public static double computeAreaOfPolygon(int numberOfSide, double side){
+        return  (numberOfSide * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / 5));
+    }
+}
