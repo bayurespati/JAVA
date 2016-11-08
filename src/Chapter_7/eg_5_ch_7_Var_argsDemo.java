ca5bc7604af5e97551a12c36936c9886a0bc2d6c
bayurespati <respatibayu@gmail.com>
2016-11-08 15:37:39 +0700

    eg_5_ch_7 var args demo

---
+++ src/Chapter_7/eg_5_ch_7_Var_argsDemo.java
@@ -0,0 +1,26 @@
+package Chapter_7;
+
+/**
+ * Created by Sletszarr on 11/8/16.
+ */
+public class eg_5_ch_7_Var_argsDemo {
+    public static void main(String[] args) {
+        printMax(34, 3, 3, 2, 56.5);
+        printMax(new double[]{1, 2, 3});
+    }
+
+    public static void printMax(double... numbers){
+        if(numbers.length == 0){
+            System.out.println("No argument passed");
+            return;
+        }
+
+        double result = numbers[0];
+
+        for(int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > result) ;
+            result = numbers[i];
+        }
+        System.out.println("The Max value is "+ result);
+    }
+}
