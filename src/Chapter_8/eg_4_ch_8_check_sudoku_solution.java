37d2c42a634f98200e8123b8f0702e92ba2dbb9c
bayurespati <respatibayu@gmail.com>
2017-01-23 15:49:35 +0700

    eg_4_ch_8 check sudoku solution

--- src/Chapter_8/eg_4_ch_8_check_sudoku_solution.java
+++ src/Chapter_8/eg_4_ch_8_check_sudoku_solution.java
@@ -51,12 +51,16 @@ public class eg_4_ch_8_check_sudoku_solution {
             }
         }
 
+
+        // Check whether grid[i][j] is unique in j's column
         for(int row = 0; row < 9; row++){
             if(row !=i && grid[row][j] == grid[i][j]){
                 return false;
             }
         }
 
+
+        // Check whether grid[i][j] is unique in the 3-by-3 box
         for (int row = (i / 3) * 3; row < (i / 3) * 3 + 3; row++){
             for(int col = (j / 3) * 3; col < (j / 3) * 3 + 3; col++){
                 if(row != i && col != j && grid[row][col] == grid[i][j]){
