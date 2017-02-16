1e0a112d4ea02df88d019115fa9b7f44016aa06a
bayurespati <respatibayu@gmail.com>
2017-02-16 21:32:39 +0700

    ex_5_ch_10 displaying the prime number

---
+++ src/Chapter_10/ex_5_ch_10_displaying_the_prime_number.java
@@ -0,0 +1,31 @@
+package Chapter_10;
+
+import Chapter_10.library_class_chapter_10.StackOfInteger;
+
+import java.util.Scanner;
+
+/**
+ * Created by bnamora on 2/16/17.
+ */
+public class ex_5_ch_10_displaying_the_prime_number {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StackOfInteger stack = new StackOfInteger();
+
+        System.out.print("Input Positive Integer : ");
+        int number = input.nextInt();
+        int factor = 2;
+
+        while (number > 1){
+            if (number % factor == 0){
+                stack.push(factor);
+                number /= factor;
+            }else
+                factor++;
+        }
+
+        while (!stack.empty()){
+            System.out.print(stack.pop()+" ");
+        }
+    }
+}
