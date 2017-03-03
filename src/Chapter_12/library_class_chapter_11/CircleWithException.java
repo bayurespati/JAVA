ce45e08b63c53b892cd7b6813f34a332915e0866
bayurespati <respatibayu@gmail.com>
2017-03-03 15:41:40 +0700

    eg_7_ch_12 class circle with excaption

---
+++ src/Chapter_12/library_class_chapter_11/CircleWithException.java
@@ -0,0 +1,38 @@
+package Chapter_12.library_class_chapter_11;
+
+
+/**
+ * Created by bnamora on 3/3/17.
+ */
+public class CircleWithException {
+    private double radius;
+    private static int numberOfObject = 0;
+
+    public CircleWithException(){
+        this(1.0);
+    }
+
+    public CircleWithException(double newRadius){
+        setRadius(newRadius);
+        numberOfObject++;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double newRadius) throws IllegalArgumentException {
+        if(newRadius >= 0)
+            radius = newRadius;
+        else
+            throw new IllegalArgumentException("Radius cannot be negative");
+    }
+
+    public static int getNumberOfObject(){
+        return numberOfObject;
+    }
+
+    public double findArea(){
+        return radius * radius * Math.PI;
+    }
+}
