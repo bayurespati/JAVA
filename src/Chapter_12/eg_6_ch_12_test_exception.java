54cc73bac3a5f9824271e01eaece6fc7fa0488bb
bayurespati <respatibayu@gmail.com>
2017-03-03 15:33:13 +0700

    eg_6_ch_12_test excaeption

---
+++ src/Chapter_12/eg_6_ch_12_test_exception.java
@@ -0,0 +1,32 @@
+package Chapter_12;
+
+/**
+ * Created by bnamora on 3/3/17.
+ */
+public class eg_6_ch_12_test_exception {
+    public static void main(String[] args) {
+        try{
+            System.out.println(sum(new int[] {1,2,3,4,5}));
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            System.out.println("\n "+ex.getMessage());
+            System.out.println("\n "+ex.toString());
+
+            System.out.println("\nTrace Info Obtained from getStackTrace");
+            StackTraceElement[] traceElements = ex.getStackTrace();
+            for(int i = 0; i < traceElements.length;i++){
+                System.out.print("Method "+traceElements[i].getMethodName());
+                System.out.print("("+traceElements[i].getClassName()+":");
+                System.out.print(traceElements[i].getLineNumber()+")");
+            }
+        }
+    }
+
+    private static int sum(int[] list){
+        int result = 0;
+        for (int i = 0; i <= list.length; i++)
+                result += list[i];
+        return result;
+    }
+}
