bdeafc41a695e29da38ea1110e88621341443102
bayurespati <respatibayu@gmail.com>
2017-03-03 15:49:26 +0700

    eg_8_ch_12 test circle with excaption

---
+++ src/Chapter_12/eg_8_ch_12_Test_Circle_with_Excaption.java
@@ -0,0 +1,20 @@
+package Chapter_12;
+
+import Chapter_12.library_class_chapter_11.CircleWithException;
+
+/**
+ * Created by bnamora on 3/3/17.
+ */
+public class eg_8_ch_12_Test_Circle_with_Excaption {
+    public static void main(String[] args) {
+        try{
+            CircleWithException c1 = new CircleWithException(5);
+            CircleWithException c2 = new CircleWithException(-5);
+            CircleWithException c3 = new CircleWithException(0);
+        }catch (IllegalArgumentException ex){
+            System.out.println(ex);
+        }
+
+        System.out.println("Number of created "+CircleWithException.getNumberOfObject());
+    }
+}
