70e5b81a40cd09b1325367f7c131bfd9dd89c594
bayurespati <respatibayu@gmail.com>
2017-03-08 11:34:05 +0700

    ex_9_ch_12 bin2dec with costum exception

---
+++ src/Chapter_12/library_class_chapter_12/BinaryFormatException.java
@@ -0,0 +1,10 @@
+package Chapter_12.library_class_chapter_12;
+
+/**
+ * Created by bnamora on 3/8/17.
+ */
+public class BinaryFormatException extends Exception {
+    public BinaryFormatException(){
+        super("This not Binary number, Please input Binary Number!!!");
+    }
+}
