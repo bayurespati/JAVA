ca60dfb4ddae6a8eba1b70962db8503523e61738
bayurespati <respatibayu@gmail.com>
2017-03-11 15:54:13 +0700

    ex_18_ch_12 refine code

--- src/Chapter_12/ex_18_ch_12_add_package_statement.java
+++ src/Chapter_12/ex_18_ch_12_add_package_statement.java
@@ -1,8 +1,6 @@
 package Chapter_12;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.io.FilenameFilter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
@@ -13,21 +11,7 @@ public class ex_18_ch_12_add_package_statement {
     public static void main(String[] args) throws Exception {
         File file = new File ("src/Chapter_12");
         ArrayList<File> listParent = getParent(file);
-
-        for(int i = 0; i < listParent.size(); i++){
-            File[] directory = listParent.get(i).listFiles();
-            for(int k = 0; k < directory.length; k++){
-                System.out.println(directory[k]);
-                    if(directory[k].isDirectory()){
-                        listParent.add(directory[k]);
-                   }else{
-                        try(PrintWriter files = new PrintWriter(directory[k])){
-                            String temp = directory[k].getParent().replaceAll("/",".");
-                            files.print("Package "+temp);
-                        }
-                    }
-            }
-        }
+        addStatement(listParent);
     }
 
     public static ArrayList<File> getParent(File file){
@@ -41,5 +25,22 @@ public class ex_18_ch_12_add_package_statement {
         }
 
         return tempFile;
+    }
+
+    public static void addStatement(ArrayList<File> listParent) throws Exception {
+        for(int i = 0; i < listParent.size(); i++){
+            File[] directory = listParent.get(i).listFiles();
+            for(int k = 0; k < directory.length; k++){
+                System.out.println(directory[k]);
+                if(directory[k].isDirectory()){
+                    listParent.add(directory[k]);
+                }else{
+                    try(PrintWriter files = new PrintWriter(directory[k])){
+                        String temp = directory[k].getParent().replaceAll("/",".");
+                        files.print("Package "+temp);
+                    }
+                }
+            }
+        }
     }
 }
