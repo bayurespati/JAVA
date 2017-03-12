0399b99002c70f76392332d2a88a694d2e37746e
bayurespati <respatibayu@gmail.com>
2017-03-12 14:37:52 +0700

    ex_28_ch_12_ rename file

---
+++ src/Chapter_12/ex_28_ch_12_rename_file.java
@@ -0,0 +1,59 @@
+package Chapter_12;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Created by bnamora on 3/12/17.
+ */
+public class ex_28_ch_12_rename_file {
+    public static void main(String[] args) {
+        File file = new File("src/Directory");
+        ArrayList<File> files = getFile(file);
+        replaceName(files);
+    }
+
+    public static ArrayList<File> getFile(File file){
+        ArrayList<File> files = new ArrayList<>();
+        File[] tempFile = file.listFiles();
+
+        for(int i = 0; i < tempFile.length; i++){
+            if(tempFile[i].isFile()) {
+                files.add(tempFile[i]);
+            }else if(tempFile[i].isDirectory()){
+                addFileFromDirectory(tempFile[i],files);
+            }
+        }
+
+        return files;
+    }
+
+    public static void addFileFromDirectory(File file,ArrayList<File> files){
+        File[] tempFile = file.listFiles();
+        for(int i = 0; i < tempFile.length; i++){
+            if(tempFile[i].isFile()) {
+                files.add(tempFile[i]);
+            }else if(tempFile[i].isDirectory()){
+                addFileFromDirectory(tempFile[i],files);
+            }
+        }
+    }
+
+    public static void replaceName(ArrayList<File> files){
+
+        for(int k = 0; k < files.size(); k++) {
+            String temp = "";
+            String nameFile = files.get(k).toString();
+            for (int i = 0; i < nameFile.length(); i++) {
+                if (Character.isDigit(nameFile.charAt(i)) && nameFile.charAt(i + 1) == '_' &&
+                        !Character.isDigit(nameFile.charAt(i - 1))) {
+                    temp += "0" + nameFile.charAt(i);
+                } else
+                    temp += nameFile.charAt(i);
+            }
+
+            File newFilename = new File(temp);
+            System.out.println( files.get(k).renameTo(newFilename) ? "Success" : "Filed" );
+        }
+    }
+}
